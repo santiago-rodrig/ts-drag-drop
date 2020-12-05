642fbe4eb8a969c3d7046426900d74c5209b4a2d
Santiago Rodriguez <santo1996.29@gmail.com>
2020-12-05 07:45:41 -0400

    define project input class

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('hey there!');
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateEl = document.getElementById('project-input');
+        this.targetNode = document.getElementById('app');
+        this._templateContents = document.importNode(this.templateEl.content, true);
+        this.formEl = this._templateContents
+            .firstElementChild;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        this.targetNode.insertAdjacentElement('afterbegin', this.formEl);
+    };
+    return ProjectInput;
+}());
+var projectInput = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log('hey there!')
+class ProjectInput {
+    templateEl: HTMLTemplateElement
+    targetNode: HTMLDivElement
+    formEl: HTMLFormElement
+    private _templateContents: DocumentFragment
+
+    constructor() {
+        this.templateEl = document.getElementById(
+            'project-input'
+        )! as HTMLTemplateElement
+
+        this.targetNode = document.getElementById('app')! as HTMLDivElement
+
+        this._templateContents = document.importNode(
+            this.templateEl.content,
+            true
+        )
+
+        this.formEl = this._templateContents
+            .firstElementChild! as HTMLFormElement
+
+        this.attach()
+    }
+
+    attach() {
+        this.targetNode.insertAdjacentElement('afterbegin', this.formEl)
+    }
+}
+
+const projectInput = new ProjectInput()
